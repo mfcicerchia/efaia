e7e3b83f03a09b41665606251fd063c6697bfd0c
alfrebenassi <alfrebenassi@gmail.com>
2012-01-04 00:56:33 +0000

    Se agrego ventana propiedades.

---
+++ src/efaia/view/AgentStateProperty.java
@@ -0,0 +1,13 @@
+package efaia.view;
+
+import org.eclipse.ui.views.properties.PropertySheet;
+
+public class AgentStateProperty extends PropertySheet {
+	
+	public static final String ID = "eFaia.view.AgentStateProperty";
+
+	public AgentStateProperty() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
