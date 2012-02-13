5432f13401532f1d7a1bbb933268ed8daf8e6ca6
alfrebenassi@gmail.com <alfrebenassi@gmail.com@175c6894-ac01-b393-9600-e2f70623b1e8>
2012-02-13 21:46:16 +0000

    Agregado File y open directory anda.

--- trunk/src/efaia/ApplicationActionBarAdvisor.java
+++ trunk/src/efaia/ApplicationActionBarAdvisor.java
@@ -1,20 +1,36 @@
 package efaia;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
+
     public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
         super(configurer);
     }
 
-    protected void makeActions(IWorkbenchWindow window) {
-    }
-
-    protected void fillMenuBar(IMenuManager menuBar) {
+    protected void makeActions(final IWorkbenchWindow window) {
     }
     
+ protected void fillMenuBar(IMenuManager menuBar) {
+    }
+    
+ protected void fillCoolBar(ICoolBarManager coolBar) {
+    }
 }
+    
